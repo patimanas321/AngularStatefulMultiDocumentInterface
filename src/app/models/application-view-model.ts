01d9bfe9cfbbf276a00dcbd4090dd9b0717a326b
Manas <patimanas321@yahoo.com>
2018-09-04 23:04:05 +0530

    application detail page changes

--- src/app/models/application-view-model.ts
+++ src/app/models/application-view-model.ts
@@ -3,6 +3,9 @@ import { Application } from "./application";
 export class ApplicationViewModel {
     actualData: Application = null;
     infoSectionVisible: boolean = false;
+    infoSectionEditMode: boolean = false;
     personalDetailsSectionVisible: boolean = false;
+    personalDetailsSectionEditMode: boolean = false;
     incomeSectionVisible: boolean = false;
+    incomeSectionEditMode: boolean = false;
 }
